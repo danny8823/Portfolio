bf356b553b0adb54029e82c93edd30833aaea5da
danny8823 <dannyyoo714@gmail.com>
2024-10-02 10:52:44 -0700

    Shorted home page introduction paragraph

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,10 +7,8 @@ function Home() {
     <div className = 'home-container'>
       <div className = 'info-container'>
         <h1 className = 'home-title'>Welcome to my portfolio.</h1>
-        <p>Thanks for stopping by, this is a webpage dedicated to showcase my skills through a display of my projects and for you to get to know me better.</p>
+        <p>Thanks for stopping by and welcome! Please take a look at my porfolio. Don't get lost. </p>
         <br/>
-        <p>I started to dive into the world of programming a couple years ago and immediately fell in love. There is nothing more enjoyable then a cup of coffee and coding up a project.</p><br/>
-        <p>I hope you enjoy checking out my projects and please feel free to contact me!</p><br/>
       </div>
       <img className = 'home-image' src = {codingImg} alt = 'clouds'/>
     </div>
