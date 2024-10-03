e0a9ac649db7dc21afa145283eec5d17e8570b45
danny8823 <dannyyoo714@gmail.com>
2024-10-03 11:53:21 -0700

    .

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,7 +8,6 @@ function NavBar() {
       <div className = 'navbar-link-container'>
         <Link to = '/' className='nav-links'>Home</Link>
         <Link to = '/projects' className='nav-links'>Projects</Link>
-        <Link to = '/resume' className='nav-links'>Resume</Link>
         {/* <Link to = '/contact'>Contact</Link> */}
         <Link to = '/about' className='nav-links'>About</Link>
       </div>
