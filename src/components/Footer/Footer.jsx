fc1467bf17663829910d73ef49f1775d6f6e9cc7
danny8823 <dannyyoo714@gmail.com>
2024-10-03 12:00:20 -0700

    Added email link icon

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Footer.css'
 import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
 
 const Footer = () => {
   return (
     <div id = 'footer-container'>
         <a href = 'https://www.linkedin.com/in/danny-yoo/' target='_blank'><FaLinkedin className = 'footer-icons'/></a>
         <a href = 'https://github.com/danny8823' target='_blank'><FaGithub className='footer-icons'/></a>
+        <a href = 'mailto:dannyyoo714@gmail.com'><HiOutlineMail className = 'footer-icons'/></a>
     </div>
   )
 }
